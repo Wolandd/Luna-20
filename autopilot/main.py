c73abead369eb4b1a91503805dc93ea839bec140
mehophd <ivivmih@gmail.com>
2024-11-25 13:01:40 +0300

    оно выходит на орбиту, код править надо будет все равно под исторический варик

--- autopilot/main.py
+++ autopilot/main.py
@@ -2,9 +2,12 @@ import krpc
 import time
 import _thread as thread
 import stageControl
+import LEO
 
 connection = krpc.connect("Connection")
 space_center = connection.space_center
 vessel = space_center.active_vessel
 
-thread.start_new_thread(stageControl.monitor, vessel)
+thread.start_new_thread(stageControl.control, tuple([vessel]))
+
+LEO.start(vessel, space_center, connection, 0.5)
