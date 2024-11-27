29544804763ab3c44b317578223425b216c6e127
mehophd <ivivmih@gmail.com>
2024-11-27 14:02:24 +0300

    вылет на Гомановскую траекторию

---
+++ autopilot/flight_to_moon.py
@@ -0,0 +1,92 @@
+import krpc
+from time import sleep
+import math
+
+def start(vessel, space_center, connection):
+    vessel.control.rcs = True
+
+    # раскрываем отсек, солнечные панели, антены и свет
+
+    vessel.control.solar_panels = True
+    vessel.control.antennas = True 
+    vessel.control.lights = True
+
+    vessel.auto_pilot.engage()
+    vessel.auto_pilot.reference_frame = vessel.orbital_reference_frame
+    vessel.auto_pilot.target_direction = (0.0, 1.0, 0.0)  # Point pro-grade
+
+    moon = space_center.bodies["Mun"]
+    moon_orbit = moon.orbit
+    moon_radius = moon_orbit.radius
+
+    target_semi_major_axis = moon_orbit.semi_major_axis # большая полуось орбиты Луны (половина длины большой оси эллипса орбиты)
+    hohmann_semi_major_axis = target_semi_major_axis / 2 # большая полуось орбиты для Гомановской траектории
+    optimal_phase_angle_rad = 2 * math.pi * (1 / (2 * (target_semi_major_axis ** 3 / hohmann_semi_major_axis ** 3) ** (1 / 2))) # оптимальный фазовый угол (по законам Кеплера) в радианах
+    optimal_phase_angle_deg = 180 - optimal_phase_angle_rad * 180 / math.pi # то же самое, но в градусах
+
+    vessel.auto_pilot.engage()
+    vessel.auto_pilot.reference_frame = vessel.orbital_reference_frame
+    vessel.auto_pilot.target_direction = (0.0, 1.0, 0.0) # направляем ракету по движению
+
+    previous_phase_angle_deg = 0
+    phase_angle_decreasing = False
+    space_center.rails_warp_factor = 0 # Start with no warp
+
+    while True:
+        current_time = space_center.ut
+        moon_position = moon_orbit.position_at(current_time, moon.reference_frame)
+        vessel_position = vessel.orbit.position_at(current_time, moon.reference_frame)
+        vessel_radius = vessel.orbit.radius
+
+        distance = math.dist(moon_position, vessel_position) # расстояние до Луны
+
+        # вычисляем текущий фазовый угол, обрабатываем ошибку из acos
+        try:
+            current_phase_angle_rad = math.acos((moon_radius**2 + vessel_radius**2 - distance**2) / (2 * moon_radius * vessel_radius))
+            current_phase_angle_deg = current_phase_angle_rad * 180 / math.pi
+        except ValueError:
+            print("Ошибка, считать не получается")
+            sleep(1)
+            continue
+
+        phase_angle_decreasing = previous_phase_angle_deg - current_phase_angle_deg > 0 # проверяем, уменьшается ли фазовый угол
+
+        phase_angle_difference = abs(current_phase_angle_deg - optimal_phase_angle_deg)
+        if phase_angle_difference <= 1 and phase_angle_decreasing:
+            break  # если разница между углами мала
+        elif phase_angle_difference > 20 and phase_angle_decreasing:
+            space_center.rails_warp_factor = 2 # если угол уменьшается, но разнциа маленькая, то ускоряем
+        elif not phase_angle_decreasing:
+            space_center.rails_warp_factor = 4 # если угол не уменьшается, то сильно ускоряем
+        else:
+            space_center.rails_warp_factor = 0
+
+
+        print(f"Фазовый угол: {current_phase_angle_deg:.2f}, Необходимый: {optimal_phase_angle_deg:.2f}")
+
+        previous_phase_angle_deg = current_phase_angle_deg
+        sleep(1)
+
+    print("Угол есть, газуем")
+    space_center.rails_warp_factor = 0
+
+
+
+    GM = vessel.orbit.body.gravitational_parameter # стандартный гравитационный параметр Земли (Кербина в нашем случае)
+    r1 = vessel.orbit.radius # радиус начальной орбиты
+    r2 = moon_orbit.semi_major_axis / 2 # целевой радиус орбиты
+    rd = r2 / r1 # отношение радиусов
+    v1 = vessel.orbit.speed # начальная орбитальная скорость
+    delta_velocity = v1*(((2 * rd) / (rd + 1))**0.5 - 1) # ускорение дельта v для перехода на эллиптическую траекторию
+    delta_deceleration = (v1 / (rd**0.5)) * (1 - (2 / (rd +1 ))**0.5) # ускорение торможения для перехода на окололунную орбиту
+
+    print(v1, rd, delta_velocity)
+    delta_velocity *= 1.05 # берем с погрешностью
+    while vessel.orbit.speed < (v1 + delta_velocity):
+        if vessel.orbit.speed / (v1 + delta_velocity) > 0.97:
+            vessel.control.throttle = 0.5
+        else:
+            vessel.control.throttle = 1
+    vessel.control.throttle = 0
+
+    # здесь будет торможение
--- autopilot/main.py
+++ autopilot/main.py
@@ -1,16 +1,20 @@
 import krpc
-import time
+from time import sleep
 import _thread as thread
 import stageControl
 import LEO
+import flight_to_moon
 
 # подключаемся, выбираем цуп и ракету нашу
 connection = krpc.connect("Connection")
 space_center = connection.space_center
 vessel = space_center.active_vessel
-
+'''
 # в параллель запускаем контроль ступеней
 thread.start_new_thread(stageControl.control, tuple([vessel]))
 
 # и выходим на НЗО
 LEO.start(vessel, space_center, connection)
+
+sleep(10)'''
+flight_to_moon.start(vessel, space_center, connection)
